09e9f94f29c2c38af5cc66be30c4ba87f3a1f198
alexandra <alexandra.chirkova@kaspi.kz>
2025-11-14 10:03:22 +0500

    add beforeEach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,7 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -18,19 +19,26 @@ public class TestBase {
         Configuration.baseUrl="https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.reopenBrowserOnFail = true;
-        Configuration.remote= System.getProperty("remote");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "142.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-       // Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+        String remote = System.getProperty("remote", "").trim();
+        if (!remote.isEmpty()) {
+            Configuration.remote = remote;
 
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
+        }
+
+    }
+
+    @BeforeEach
+    void addListener(){
         SelenideLogger.addListener("allure", new AllureSelenide()
                 .screenshots(true)
                 .savePageSource(true));
